de9061522214168121d5baec56ca7cbbe6ab301e
HoJoonKim <hojoon.kim@snu.ac.kr>
2025-02-21 19:57:55 +0900

    added scripts

---
+++ scripts/profile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get the absolute path to the directory containing this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Get the project base directory
+BASE_DIR="$SCRIPT_DIR/.."
+
+RESULT_DIR="$BASE_DIR/profile_results"
+mkdir -p "$RESULT_DIR"
+
+# FIXME
+MODEL="Llama-3.1-8b-Instruct"
+BIT="4"
+DECODE="quantlut_sym"
+
+RESULT_FILE="$MODEL.$BIT.$DECODE.nsys-rep"
+
+RUN_CMD="python -m eval.eval_ppl  --hf_path relaxml/Llama-3.1-8b-Instruct-QTIP-4Bit"
+
+nsys profile --capture-range=cudaProfilerApi \
+  --output="$RESULT_DIR/$RESULT_FILE" \
+  --force-overwrite=true \
+  ${RUN_CMD}
+
+
+
